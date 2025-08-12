4ca2f8962d58b402b78920948b12197174d6f75c
Trust-Worthy <jondeveloper0@gmail.com>
2025-08-12 14:22:54 -0600

    Connected all UI components to the View controller file.

--- 102-prework/102-prework/ViewController.swift
+++ 102-prework/102-prework/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var yearSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func introduceSelfDidTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+    }
+    
 }
 
